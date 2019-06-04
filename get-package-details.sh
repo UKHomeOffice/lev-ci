b6efcd2930e0ef2f1dd41b3a48fc28e5be16cf7c
Robin Knipe <robin.knipe@gmail.com>
2019-06-04 11:53:20 +0100

    Add a generic way to get node package version vars

---
+++ get-package-details.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export VERSION=`cat package.json | jq -r '.version'`
+export MAJOR=`echo -n "${VERSION}" | awk -F '.' '{print $1}'`
+export MINOR=`echo -n "${VERSION}" | awk -F '.' '{print $2}'`
+export PATCH=`echo -n "${VERSION}" | awk -F '.' '{print $3}'`
