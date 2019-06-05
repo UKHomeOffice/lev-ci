5f121bbddbb6274199b24eb07fc218753d88bd9e
Robin Knipe <robin.knipe@gmail.com>
2019-06-05 17:02:54 +0100

    Optionally take package file location from param
    ...if provided, otherwise try the current directory as usual.

--- get-package-details.sh
+++ get-package-details.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [ -f package.json ] ; then
-  export VERSION=`cat package.json | jq -r '.version'`
+package=${1:-package.json}
+
+if [ -f ${package} ] ; then
+  export VERSION=`cat ${package} | jq -r '.version'`
   export MAJOR=`echo -n "${VERSION}" | awk -F '.' '{print $1}'`
   export MINOR=`echo -n "${VERSION}" | awk -F '.' '{print $2}'`
   export PATCH=`echo -n "${VERSION}" | awk -F '.' '{print $3}'`
