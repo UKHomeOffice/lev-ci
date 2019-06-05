2de8c4f71cb1752a2d8fe8601f2a76b7e223672d
Robin Knipe <robin.knipe@gmail.com>
2019-06-05 17:01:54 +0100

    Only extract package version if the file exists

--- get-package-details.sh
+++ get-package-details.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export VERSION=`cat package.json | jq -r '.version'`
-export MAJOR=`echo -n "${VERSION}" | awk -F '.' '{print $1}'`
-export MINOR=`echo -n "${VERSION}" | awk -F '.' '{print $2}'`
-export PATCH=`echo -n "${VERSION}" | awk -F '.' '{print $3}'`
+if [ -f package.json ] ; then
+  export VERSION=`cat package.json | jq -r '.version'`
+  export MAJOR=`echo -n "${VERSION}" | awk -F '.' '{print $1}'`
+  export MINOR=`echo -n "${VERSION}" | awk -F '.' '{print $2}'`
+  export PATCH=`echo -n "${VERSION}" | awk -F '.' '{print $3}'`
+fi
